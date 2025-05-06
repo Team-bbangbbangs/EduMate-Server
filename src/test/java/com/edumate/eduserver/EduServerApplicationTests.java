84c39516b392959158436e8c9a4b29328efc65b6
kgy1008 <gayeon1008@g.skku.edu>
2025-05-06 12:11:50 +0900

    [init] initial setting

---
+++ src/test/java/com/edumate/eduserver/EduServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edumate.eduserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
