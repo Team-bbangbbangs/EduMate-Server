c271845be6f20dcb3ffaff1b73ff94fef81615f7
kgy1008 <gayeon1008@g.skku.edu>
2025-05-25 14:49:27 +0900

    [feat] RestDocs 정상 생성 확인을 위한 임시 코드 생성

---
+++ src/main/java/com/edumate/eduserver/TestRequest.java
@@ -0,0 +1,7 @@
+package com.edumate.eduserver;
+
+public record TestRequest(
+        String testStr,
+        int testInt
+) {
+}
---
+++ src/main/java/com/edumate/eduserver/TestResponse.java
@@ -0,0 +1,7 @@
+package com.edumate.eduserver;
+
+public record TestResponse(
+        String testStr,
+        int testInt
+) {
+}
