47576843f50afdff9e468f59c78fda99409a59ce
kgy1008 <gayeon1008@g.skku.edu>
2025-06-05 18:08:11 +0900

    [feat] 유효 교육청 이메일 테이블 생성

---
+++ src/main/java/com/edumate/eduserver/auth/domain/ValidEmail.java
@@ -0,0 +1,24 @@
+package com.edumate.eduserver.auth.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ValidEmail {
+
+    @Id
+    @Column(name = "email_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String validEmail;
+}
