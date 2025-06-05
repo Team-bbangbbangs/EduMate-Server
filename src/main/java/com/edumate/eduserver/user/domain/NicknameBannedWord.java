1b1ca80f487a70c1b1463c6557c6470223a00be8
kgy1008 <gayeon1008@g.skku.edu>
2025-06-05 18:09:27 +0900

    [feat] 닉네임 금칙어 테이블 생성

---
+++ src/main/java/com/edumate/eduserver/user/domain/NicknameBannedWord.java
@@ -0,0 +1,25 @@
+package com.edumate.eduserver.user.domain;
+
+import com.edumate.eduserver.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NicknameBannedWord extends BaseEntity {
+
+    @Id
+    @Column(name = "word_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String word;
+}
