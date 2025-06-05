4a8274fe11bda7a7c18bdf6340bc37f5cc1facdf
kgy1008 <gayeon1008@g.skku.edu>
2025-06-05 18:10:36 +0900

    [feat] 생활기록부 메타정보 테이블 생성

---
+++ src/main/java/com/edumate/eduserver/studentrecord/domain/MemberStudentRecord.java
@@ -0,0 +1,35 @@
+package com.edumate.eduserver.studentrecord.domain;
+
+import com.edumate.eduserver.user.domain.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberStudentRecord {
+
+    @Id
+    @Column(name = "student_record_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Member member;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StudentRecordType studentRecordType;
+
+    @Column(nullable = false)
+    private String semester;
+}
