b293c8a78c800080e17798cefc83a6e9317a8406
kgy1008 <gayeon1008@g.skku.edu>
2025-06-05 18:54:54 +0900

    [dependency] h2 의존성 추가

--- src/test/java/com/edumate/eduserver/EduServerApplicationTests.java
+++ src/test/java/com/edumate/eduserver/EduServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.edumate.eduserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EduServerApplicationTests {
 
 	@Test
