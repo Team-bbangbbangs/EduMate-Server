3cc7e6440e14d56392a6aa7b55bc26dfb3bb7e23
kgy1008 <gayeon1008@g.skku.edu>
2025-06-06 00:11:44 +0900

    [refac] admin 권한 추가

--- src/main/java/com/edumate/eduserver/user/domain/Role.java
+++ src/main/java/com/edumate/eduserver/user/domain/Role.java
@@ -1,6 +1,12 @@
 package com.edumate.eduserver.user.domain;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum Role {
-    TEACHER,
-    PENDING_TEACHER
+    TEACHER("TEACHER"),
+    PENDING_TEACHER("PENDING_TEACHER"),
+    ADMIN("ADMIN");
+
+    private final String name;
 }
