780518d3eb17ac93999bd22a17d1c37512a51ab7
kgy1008 <gayeon1008@g.skku.edu>
2025-06-06 01:25:23 +0900

    [refac] Custom 에러 코드 수정

--- src/main/java/com/edumate/eduserver/auth/exception/code/AuthErrorCode.java
+++ src/main/java/com/edumate/eduserver/auth/exception/code/AuthErrorCode.java
@@ -7,10 +7,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
-    INVALID_ACCESS_TOKEN_VALUE("EDMT-40401", "유효하지 않은 엑세스 토큰입니다."),
-    EXPIRED_ACCESS_TOKEN("EDMT-40402", "이미 만료된 엑세스 토큰입니다."),
-    EXPIRED_REFRESH_TOKEN("EDMT-40403", "이미 만료된 리프레시 토큰입니다."),
-    INVALID_REFRESH_TOKEN_VALUE("EDMT-40404", "유효하지 않은 리프레시 토큰입니다.");
+    INVALID_ACCESS_TOKEN_VALUE("EDMT-40101", "유효하지 않은 엑세스 토큰입니다."),
+    EXPIRED_ACCESS_TOKEN("EDMT-40102", "이미 만료된 엑세스 토큰입니다."),
+    EXPIRED_REFRESH_TOKEN("EDMT-40103", "이미 만료된 리프레시 토큰입니다."),
+    INVALID_REFRESH_TOKEN_VALUE("EDMT-40104", "유효하지 않은 리프레시 토큰입니다.");
 
     private final String code;
     private final String message;
