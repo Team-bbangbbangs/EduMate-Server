4e6df412e7558b9291760e7be24a204a94ca182b
kgy1008 <gayeon1008@g.skku.edu>
2025-06-05 18:11:16 +0900

    [feat] 생활기록부 상세내용 테이블 생성

---
+++ src/main/java/com/edumate/eduserver/studentrecord/domain/StudentRecordDetail.java
@@ -0,0 +1,38 @@
+package com.edumate.eduserver.studentrecord.domain;
+
+import com.edumate.eduserver.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StudentRecordDetail extends BaseEntity {
+
+    @Id
+    @Column(name = "student_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private MemberStudentRecord memberStudentRecord;
+
+    @Column(nullable = false)
+    private String studentNumber;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private int byteCount;
+}
