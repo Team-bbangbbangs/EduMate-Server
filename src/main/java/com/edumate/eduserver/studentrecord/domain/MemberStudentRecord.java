7f69f5f3325a7c6e8332c377ff87fd2031947dd4
kgy1008 <gayeon1008@g.skku.edu>
2025-06-05 19:11:10 +0900

    [refac] 지연 로딩으로 설정

--- src/main/java/com/edumate/eduserver/studentrecord/domain/MemberStudentRecord.java
+++ src/main/java/com/edumate/eduserver/studentrecord/domain/MemberStudentRecord.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -23,7 +24,7 @@ public class MemberStudentRecord {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
 
     @Column(nullable = false)
