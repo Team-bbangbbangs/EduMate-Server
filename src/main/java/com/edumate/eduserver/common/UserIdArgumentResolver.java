31da0c60b26a7b8c5e7b0c76cbb6550d018bdedc
Gayeon Kim <gayeon1008@g.skku.edu>
2025-06-06 22:36:28 +0900

    [EDMT-120] JWT에서 UserId를 파싱하는 argument resolver 구현 (#9)
    * [feat] 사용자 ID를 주입받기 위한 @UserId 어노테이션 추가
    
    * [dependency] security 의존성 추가
    
    * [feat] @UserId 어노테이션을 처리하는 ArgumentResolver 구현
    
    * [feat] @UserId 파라미터 리졸버를 MVC에 등록하는 WebConfig 클래스 추가
    
    * [refac] long 타입 파라미터도 인식하도록 개선
    
    * [refac] 개행 수정

---
+++ src/main/java/com/edumate/eduserver/common/UserIdArgumentResolver.java
@@ -0,0 +1,27 @@
+package com.edumate.eduserver.common;
+
+import com.edumate.eduserver.common.annotation.UserId;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class UserIdArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(final MethodParameter parameter) {
+        boolean hasUserIdAnnotation = parameter.hasParameterAnnotation(UserId.class);
+        boolean isLongType = parameter.getParameterType().equals(Long.class) || parameter.getParameterType().equals(long.class);
+        return hasUserIdAnnotation && isLongType;
+    }
+
+    @Override
+    public Object resolveArgument(final MethodParameter parameter, final ModelAndViewContainer mavContainer,
+                                  final NativeWebRequest webRequest, final WebDataBinderFactory binderFactory) throws Exception {
+        return SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+}
