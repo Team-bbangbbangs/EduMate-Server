1868c5174beb656dfa6f4fe823639d6a088beee2
kgy1008 <gayeon1008@g.skku.edu>
2025-06-06 13:19:48 +0900

    [feat] @UserId 어노테이션을 처리하는 ArgumentResolver 구현

---
+++ src/main/java/com/edumate/eduserver/common/UserIdArgumentResolver.java
@@ -0,0 +1,25 @@
+package com.edumate.eduserver.common;
+
+import com.edumate.eduserver.common.annotation.UserId;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class UserIdArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(final MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(UserId.class) && parameter.getParameterType().equals(Long.class);
+    }
+
+    @Override
+    public Object resolveArgument(final MethodParameter parameter, final ModelAndViewContainer mavContainer,
+                                  final NativeWebRequest webRequest, final WebDataBinderFactory binderFactory) throws Exception {
+        return SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+}
