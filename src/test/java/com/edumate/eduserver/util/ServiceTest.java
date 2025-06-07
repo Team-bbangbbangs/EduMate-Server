c90ae2923b8839e38b23d85aab460043ddca7913
kgy1008 <gayeon1008@g.skku.edu>
2025-06-08 04:25:17 +0900

    [fix] Fixture 간 연관관계 주입 방식 변경하여 영속성 오류 해결

--- src/test/java/com/edumate/eduserver/util/ServiceTest.java
+++ src/test/java/com/edumate/eduserver/util/ServiceTest.java
@@ -1,14 +1,16 @@
 package com.edumate.eduserver.util;
 
+import com.edumate.eduserver.studentrecord.domain.MemberStudentRecord;
 import com.edumate.eduserver.studentrecord.repository.MemberStudentRecordRepository;
 import com.edumate.eduserver.studentrecord.repository.StudentRecordDetailRepository;
+import com.edumate.eduserver.subject.domain.Subject;
 import com.edumate.eduserver.subject.repository.SubjectRepository;
+import com.edumate.eduserver.user.domain.Member;
 import com.edumate.eduserver.user.repository.MemberRepository;
 import com.edumate.eduserver.util.fixture.MemberFixture;
 import com.edumate.eduserver.util.fixture.MemberStudentRecordFixture;
 import com.edumate.eduserver.util.fixture.StudentRecordDetailFixture;
 import com.edumate.eduserver.util.fixture.SubjectFixture;
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +20,8 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 public class ServiceTest {
+
+    protected Member defaultTeacher;
 
     @Autowired
     private DbCleaner dbCleaner;
@@ -37,17 +41,10 @@ public class ServiceTest {
     @BeforeEach
     void resetDb() {
         dbCleaner.truncateEveryTable();
-        subjectRepository.saveAll(List.of(SubjectFixture.KOREAN, SubjectFixture.MATH, SubjectFixture.ENGLISH));
-        memberRepository.saveAll(List.of(MemberFixture.TEACHER_01, MemberFixture.TEACHER_02, MemberFixture.TEACHER_03));
-        memberStudentRecordRepository.saveAll(List.of(
-                MemberStudentRecordFixture.STUDENT_RECORD_01,
-                MemberStudentRecordFixture.STUDENT_RECORD_02,
-                MemberStudentRecordFixture.STUDENT_RECORD_03)
-        );
-        studentRecordDetailRepository.saveAll(List.of(
-                StudentRecordDetailFixture.STUDENT_RECORD_DETAIL_01,
-                StudentRecordDetailFixture.STUDENT_RECORD_DETAIL_02,
-                StudentRecordDetailFixture.STUDENT_RECORD_DETAIL_03)
-        );
+
+        Subject korean = subjectRepository.save(SubjectFixture.korean());
+        defaultTeacher = memberRepository.save(MemberFixture.teacher01(korean));
+        MemberStudentRecord record01 = memberStudentRecordRepository.save(MemberStudentRecordFixture.studentRecord01(defaultTeacher));
+        studentRecordDetailRepository.save(StudentRecordDetailFixture.detail01(record01));
     }
 }
--- src/test/java/com/edumate/eduserver/util/fixture/StudentRecordDetailFixture.java
+++ src/test/java/com/edumate/eduserver/util/fixture/StudentRecordDetailFixture.java
@@ -1,25 +1,28 @@
 package com.edumate.eduserver.util.fixture;
 
-import static com.edumate.eduserver.util.fixture.MemberStudentRecordFixture.STUDENT_RECORD_01;
-import static com.edumate.eduserver.util.fixture.MemberStudentRecordFixture.STUDENT_RECORD_02;
-import static com.edumate.eduserver.util.fixture.MemberStudentRecordFixture.STUDENT_RECORD_03;
-
+import com.edumate.eduserver.studentrecord.domain.MemberStudentRecord;
 import com.edumate.eduserver.studentrecord.domain.StudentRecordDetail;
 
 public class StudentRecordDetailFixture {
 
-    public static final StudentRecordDetail STUDENT_RECORD_DETAIL_01 = StudentRecordDetail.create(
-            STUDENT_RECORD_01, "2020314912", "김가연",
-            "학생은 수업에 적극적으로 참여하고 있으며, 학습 능력이 뛰어납니다.", 10
-    );
+    public static StudentRecordDetail detail01(MemberStudentRecord studentRecord) {
+        return StudentRecordDetail.create(
+                studentRecord, "2020314912", "김가연",
+                "학생은 수업에 적극적으로 참여하고 있으며, 학습 능력이 뛰어납니다.", 10
+        );
+    }
 
-    public static final StudentRecordDetail STUDENT_RECORD_DETAIL_02 = StudentRecordDetail.create(
-            STUDENT_RECORD_02, "2020314913", "이승섭",
-            "학생은 수업에 적극적으로 참여하고 있으며, 학습 능력이 우수합니다.", 9
-    );
+    public static StudentRecordDetail detail02(MemberStudentRecord studentRecord) {
+        return StudentRecordDetail.create(
+                studentRecord, "2020314913", "이승섭",
+                "학생은 수업에 적극적으로 참여하고 있으며, 학습 능력이 우수합니다.", 9
+        );
+    }
 
-    public static final StudentRecordDetail STUDENT_RECORD_DETAIL_03 = StudentRecordDetail.create(
-            STUDENT_RECORD_03, "2020314914", "유태근",
-            "학생의 능력에 대한 상세한 의견입니다. 학생은 수업에 적극적으로 참여하고 있으며, 학습 능력이 양호합니다.", 8
-    );
+    public static StudentRecordDetail detail03(MemberStudentRecord studentRecord) {
+        return StudentRecordDetail.create(
+                studentRecord, "2020314914", "유태근",
+                "학생의 능력에 대한 상세한 의견입니다. 학생은 수업에 적극적으로 참여하고 있으며, 학습 능력이 양호합니다.", 8
+        );
+    }
 }
