5d2feaa3bf7e643a067ab9baa8e1e61e32c9acbd
kgy1008 <gayeon1008@g.skku.edu>
2025-06-08 01:50:56 +0900

    [feat] Repository 테스트용 베이스 클래스 구성

---
+++ src/test/java/com/edumate/eduserver/util/RepositoryTest.java
@@ -0,0 +1,8 @@
+package com.edumate.eduserver.util;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest(excludeAutoConfiguration = {FlywayAutoConfiguration.class})
+public class RepositoryTest {
+}
