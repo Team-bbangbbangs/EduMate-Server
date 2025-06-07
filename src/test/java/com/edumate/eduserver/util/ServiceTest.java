9639d9a03b46394742c29e404d970039c452d07a
kgy1008 <gayeon1008@g.skku.edu>
2025-06-08 02:13:41 +0900

    [feat] service 테스트용 베이스 클래스 생성

--- src/test/java/com/edumate/eduserver/util/ServiceTest.java
+++ src/test/java/com/edumate/eduserver/util/ServiceTest.java
@@ -1,8 +1,12 @@
 package com.edumate.eduserver.util;
 
+import com.edumate.eduserver.studentrecord.repository.MemberStudentRecordRepository;
+import com.edumate.eduserver.studentrecord.repository.StudentRecordDetailRepository;
 import com.edumate.eduserver.subject.repository.SubjectRepository;
 import com.edumate.eduserver.user.repository.MemberRepository;
 import com.edumate.eduserver.util.fixture.MemberFixture;
+import com.edumate.eduserver.util.fixture.MemberStudentRecordFixture;
+import com.edumate.eduserver.util.fixture.StudentRecordDetailFixture;
 import com.edumate.eduserver.util.fixture.SubjectFixture;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,10 +28,26 @@ public class ServiceTest {
     @Autowired
     private SubjectRepository subjectRepository;
 
+    @Autowired
+    private MemberStudentRecordRepository memberStudentRecordRepository;
+
+    @Autowired
+    private StudentRecordDetailRepository studentRecordDetailRepository;
+
     @BeforeEach
     void resetDb() {
         dbCleaner.truncateEveryTable();
         subjectRepository.saveAll(List.of(SubjectFixture.KOREAN, SubjectFixture.MATH, SubjectFixture.ENGLISH));
-        memberRepository.saveAll(List.of(MemberFixture.TEACHER01, MemberFixture.TEACHER02, MemberFixture.TEACHER03));
+        memberRepository.saveAll(List.of(MemberFixture.TEACHER_01, MemberFixture.TEACHER_02, MemberFixture.TEACHER_03));
+        memberStudentRecordRepository.saveAll(List.of(
+                MemberStudentRecordFixture.STUDENT_RECORD_01,
+                MemberStudentRecordFixture.STUDENT_RECORD_02,
+                MemberStudentRecordFixture.STUDENT_RECORD_03)
+        );
+        studentRecordDetailRepository.saveAll(List.of(
+                StudentRecordDetailFixture.STUDENT_RECORD_DETAIL_01,
+                StudentRecordDetailFixture.STUDENT_RECORD_DETAIL_02,
+                StudentRecordDetailFixture.STUDENT_RECORD_DETAIL_03)
+        );
     }
 }
