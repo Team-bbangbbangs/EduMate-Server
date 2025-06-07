db93eb40e3be0a22ac8e50f436f90510f8f1f0c4
kgy1008 <gayeon1008@g.skku.edu>
2025-06-07 19:05:12 +0900

    [fix] enum 이름이 아닌 value 값으로 매핑하도록 수정

--- src/main/java/com/edumate/eduserver/common/StudentRecordTypeConverter.java
+++ src/main/java/com/edumate/eduserver/common/StudentRecordTypeConverter.java
@@ -1,8 +1,9 @@
 package com.edumate.eduserver.common;
 
-import com.edumate.eduserver.common.code.BusinessErrorCode;
 import com.edumate.eduserver.studentrecord.domain.StudentRecordType;
 import com.edumate.eduserver.studentrecord.exception.RecordTypeNotFoundException;
+import com.edumate.eduserver.studentrecord.exception.code.StudentRecordErrorCode;
+import java.util.Arrays;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
@@ -11,10 +12,9 @@ public class StudentRecordTypeConverter implements Converter<String, StudentReco
 
     @Override
     public StudentRecordType convert(final String source) {
-        try {
-            return StudentRecordType.valueOf(source.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            throw new RecordTypeNotFoundException(BusinessErrorCode.BAD_REQUEST);
-        }
+        return Arrays.stream(StudentRecordType.values())
+                .filter(type -> type.getValue().equalsIgnoreCase(source))
+                .findFirst()
+                .orElseThrow(() -> new RecordTypeNotFoundException(StudentRecordErrorCode.RECORD_TYPE_NOT_FOUND));
     }
 }
--- src/main/java/com/edumate/eduserver/studentrecord/domain/StudentRecordType.java
+++ src/main/java/com/edumate/eduserver/studentrecord/domain/StudentRecordType.java
@@ -1,11 +1,13 @@
 package com.edumate.eduserver.studentrecord.domain;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public enum StudentRecordType {
 
-    ABILITY_DETAIL("ACTIVITY"),
+    ABILITY_DETAIL("ABILITY"),
     BEHAVIOR_OPINION("BEHAVIOR"),
     CREATIVE_ACTIVITY_CAREER("CREATIVE-CAREER"),
     CREATIVE_ACTIVITY_AUTONOMY("CREATIVE-AUTONOMY"),
