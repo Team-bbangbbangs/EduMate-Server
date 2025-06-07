6fc6a0db41acce61f40535a1f8cf56de12e6b296
kgy1008 <gayeon1008@g.skku.edu>
2025-06-08 01:50:50 +0900

    [feat] Controller 테스트용 베이스 클래스 구성

---
+++ src/test/java/com/edumate/eduserver/util/ControllerTest.java
@@ -0,0 +1,26 @@
+package com.edumate.eduserver.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+@ActiveProfiles("test")
+public class ControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    protected String toJson(Object obj) throws Exception {
+        return objectMapper.writeValueAsString(obj);
+    }
+
+    protected <T> T fromJson(String json, Class<T> clazz) throws Exception {
+        return objectMapper.readValue(json, clazz);
+    }
+}
