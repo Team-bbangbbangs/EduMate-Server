3ae71e4d6aa191fb1a7cce9e7aa77bb9f23f8196
kgy1008 <gayeon1008@g.skku.edu>
2025-06-08 01:50:32 +0900

    [feat] Subject 픽스쳐 생성

---
+++ src/test/java/com/edumate/eduserver/util/fixture/SubjectFixture.java
@@ -0,0 +1,10 @@
+package com.edumate.eduserver.util.fixture;
+
+import com.edumate.eduserver.subject.domain.Subject;
+
+public class SubjectFixture {
+
+    public static final Subject KOREAN = Subject.create("국어");
+    public static final Subject MATH = Subject.create("수학");
+    public static final Subject ENGLISH = Subject.create("영어");
+}
