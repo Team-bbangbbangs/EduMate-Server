f193ca9fe160ae778a8a424f18606fef0cebae49
kgy1008 <gayeon1008@g.skku.edu>
2025-06-07 23:53:47 +0900

    [refac] 예외 코드에 맞는 부모 클래스를 상속하도록 수정

--- src/main/java/com/edumate/eduserver/studentrecord/exception/StudentRecordDetailNotFoundException.java
+++ src/main/java/com/edumate/eduserver/studentrecord/exception/StudentRecordDetailNotFoundException.java
@@ -1,9 +1,9 @@
 package com.edumate.eduserver.studentrecord.exception;
 
-import com.edumate.eduserver.common.exception.BadRequestException;
+import com.edumate.eduserver.common.exception.NotFoundException;
 import com.edumate.eduserver.studentrecord.exception.code.StudentRecordErrorCode;
 
-public class StudentRecordDetailNotFoundException extends BadRequestException {
+public class StudentRecordDetailNotFoundException extends NotFoundException {
 
     public StudentRecordDetailNotFoundException(final StudentRecordErrorCode errorCode) {
         super(errorCode);
