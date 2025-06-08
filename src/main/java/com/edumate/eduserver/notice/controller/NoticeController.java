9c68ac54af751d69f66696b7942aaad8aa351c9c
TaegeunYou <dbxorms2310@naver.com>
2025-06-08 19:52:14 +0900

    [refac] 컨트롤러 리소스 이름 분리

--- src/main/java/com/edumate/eduserver/notice/controller/NoticeController.java
+++ src/main/java/com/edumate/eduserver/notice/controller/NoticeController.java
@@ -2,9 +2,9 @@ package com.edumate.eduserver.notice.controller;
 
 import com.edumate.eduserver.common.ApiResponse;
 import com.edumate.eduserver.common.code.CommonSuccessCode;
-import com.edumate.eduserver.notice.controller.response.NoticesGetResponse;
 import com.edumate.eduserver.notice.domain.NoticeCategory;
 import com.edumate.eduserver.notice.facade.NoticeFacade;
+import com.edumate.eduserver.notice.facade.response.NoticesGetResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +12,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/notices")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class NoticeController {
 
     private final NoticeFacade noticeFacade;
 
-    @GetMapping
+    @GetMapping("/notices")
     public ApiResponse<NoticesGetResponse> getNotices(
             @RequestParam(name = "categoryId", required = false) Integer categoryId,
             @RequestParam(name = "page", required = false, defaultValue = "1") int page
