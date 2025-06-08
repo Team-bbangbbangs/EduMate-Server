91abd7aaf31b362feafac21f92844d83ed54616b
TaegeunYou <dbxorms2310@naver.com>
2025-06-08 19:52:57 +0900

    [refac] 페이지 파라미터 밸리데이션 추가

--- src/main/java/com/edumate/eduserver/notice/controller/NoticeController.java
+++ src/main/java/com/edumate/eduserver/notice/controller/NoticeController.java
@@ -5,6 +5,7 @@ import com.edumate.eduserver.common.code.CommonSuccessCode;
 import com.edumate.eduserver.notice.domain.NoticeCategory;
 import com.edumate.eduserver.notice.facade.NoticeFacade;
 import com.edumate.eduserver.notice.facade.response.NoticesGetResponse;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,7 @@ public class NoticeController {
     @GetMapping("/notices")
     public ApiResponse<NoticesGetResponse> getNotices(
             @RequestParam(name = "categoryId", required = false) Integer categoryId,
-            @RequestParam(name = "page", required = false, defaultValue = "1") int page
+            @RequestParam(name = "page", required = false, defaultValue = "1") @Min(1) int page
     ) {
         NoticeCategory category = NoticeCategory.fromId(categoryId);
         return ApiResponse.success(
