f0b06aa2b4dd201c9a6b8c984c31b079af27d21e
TaegeunYou <dbxorms2310@naver.com>
2025-06-08 19:53:48 +0900

    [fix] 유효하지 않은 카테고리 에러명 변경

--- src/main/java/com/edumate/eduserver/notice/exception/InvalidNoticeCategoryException.java
+++ src/main/java/com/edumate/eduserver/notice/exception/InvalidNoticeCategoryException.java
@@ -3,9 +3,9 @@ package com.edumate.eduserver.notice.exception;
 import com.edumate.eduserver.common.exception.BadRequestException;
 import com.edumate.eduserver.notice.exception.code.NoticeErrorCode;
 
-public class NoticeCategoryNotFoundException extends BadRequestException {
+public class InvalidNoticeCategoryException extends BadRequestException {
 
-    public NoticeCategoryNotFoundException(final NoticeErrorCode errorCode) {
+    public InvalidNoticeCategoryException(final NoticeErrorCode errorCode) {
         super(errorCode);
     }
 }
