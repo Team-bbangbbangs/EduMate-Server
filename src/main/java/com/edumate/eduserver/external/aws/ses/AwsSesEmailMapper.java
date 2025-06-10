9feb61b97bc1311803ca4d912a1f07affbd79ed6
kgy1008 <gayeon1008@g.skku.edu>
2025-06-10 16:00:06 +0900

    [refac] 이메일 전송 시 UI 수정

--- src/main/java/com/edumate/eduserver/external/aws/ses/AwsSesEmailMapper.java
+++ src/main/java/com/edumate/eduserver/external/aws/ses/AwsSesEmailMapper.java
@@ -1,28 +1,19 @@
 package com.edumate.eduserver.external.aws.ses;
 
-import com.amazonaws.services.simpleemail.model.Body;
-import com.amazonaws.services.simpleemail.model.Content;
-import com.amazonaws.services.simpleemail.model.Destination;
-import com.amazonaws.services.simpleemail.model.Message;
-import com.amazonaws.services.simpleemail.model.SendEmailRequest;
-import java.nio.charset.StandardCharsets;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Component
+@RequiredArgsConstructor
 public class AwsSesEmailMapper {
 
-    private static final String SENDER = "libra001008@gmail.com";
+    private final TemplateEngine templateEngine;
 
-    public SendEmailRequest toSendEmailRequest(final EmailSendDto emailSendDto) {
-        Destination destination = new Destination().withToAddresses(emailSendDto.receiver());
-        Message message = new Message().withSubject(createContent(emailSendDto.subject()))
-                .withBody(new Body().withHtml(createContent(emailSendDto.content())));
-        return new SendEmailRequest().withSource(SENDER)
-                .withDestination(destination)
-                .withMessage(message);
-    }
-
-    private Content createContent(final String text) {
-        return new Content().withCharset(StandardCharsets.UTF_8.name()).withData(text);
+    public String buildVerificationEmail(final String verificationLink) {
+        Context context = new Context();
+        context.setVariable("verificationLink", verificationLink);
+        return templateEngine.process("email-verification", context);
     }
 }
