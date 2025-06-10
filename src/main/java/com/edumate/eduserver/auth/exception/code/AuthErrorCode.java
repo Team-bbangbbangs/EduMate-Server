e313243f00d1a33d5dc2a1201cab16415a9dd8db
kgy1008 <gayeon1008@g.skku.edu>
2025-06-10 15:54:17 +0900

    [feat] 이메일 전송 예외 생성

--- src/main/java/com/edumate/eduserver/auth/exception/code/AuthErrorCode.java
+++ src/main/java/com/edumate/eduserver/auth/exception/code/AuthErrorCode.java
@@ -11,7 +11,10 @@ public enum AuthErrorCode implements ErrorCode {
     INVALID_ACCESS_TOKEN_VALUE("EDMT-4010101", "유효하지 않은 엑세스 토큰입니다."),
     EXPIRED_ACCESS_TOKEN("EDMT-4010102", "이미 만료된 엑세스 토큰입니다."),
     EXPIRED_REFRESH_TOKEN("EDMT-4010103", "이미 만료된 리프레시 토큰입니다."),
-    INVALID_REFRESH_TOKEN_VALUE("EDMT-4010104", "유효하지 않은 리프레시 토큰입니다.");
+    INVALID_REFRESH_TOKEN_VALUE("EDMT-4010104", "유효하지 않은 리프레시 토큰입니다."),
+
+    // 502 Bad Gateway
+    EMAIL_SEND_FAILED("EDMT-5020105", "이메일 전송에 실패했습니다."),;
 
     private final String code;
     private final String message;
