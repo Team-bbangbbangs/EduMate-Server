e1fa3c6c6df12790dc94f129cf2345debb42c783
kgy1008 <gayeon1008@g.skku.edu>
2025-06-12 03:30:58 +0900

    [refac] SecureRandom 객체 캐싱

--- src/main/java/com/edumate/eduserver/auth/service/RandomCodeGenerator.java
+++ src/main/java/com/edumate/eduserver/auth/service/RandomCodeGenerator.java
@@ -8,14 +8,14 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class RandomCodeGenerator {
 
+    private static final SecureRandom RANDOM = new SecureRandom();
     private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     private static final int CODE_LENGTH = 6;
 
     public String generate() {
-        SecureRandom random = new SecureRandom();
         StringBuilder builder = new StringBuilder(CODE_LENGTH);
         for (int i = 0; i < CODE_LENGTH; i++) {
-            int index = random.nextInt(CHARACTERS.length());
+            int index = RANDOM.nextInt(CHARACTERS.length());
             builder.append(CHARACTERS.charAt(index));
         }
         return builder.toString();
