bf5c8a7ca76cfcb2250f46a5db8e912d746991f8
kgy1008 <gayeon1008@g.skku.edu>
2025-06-13 02:42:09 +0900

    [feat] Jwt 인증 실패 처리 로직 추가

--- src/main/java/com/edumate/eduserver/auth/exception/code/AuthErrorCode.java
+++ src/main/java/com/edumate/eduserver/auth/exception/code/AuthErrorCode.java
@@ -17,6 +17,7 @@ public enum AuthErrorCode implements ErrorCode {
     EXPIRED_ACCESS_TOKEN("EDMT-4010102", "이미 만료된 엑세스 토큰입니다."),
     EXPIRED_REFRESH_TOKEN("EDMT-4010103", "이미 만료된 리프레시 토큰입니다."),
     INVALID_REFRESH_TOKEN_VALUE("EDMT-4010104", "유효하지 않은 리프레시 토큰입니다."),
+    UNAUTHORIZED("EDMT-4010110", "인증되지 않은 사용자입니다."),
 
     // 404 Not Found
     AUTH_CODE_NOT_FOUND("EDMT-4040107", "유효한 인증 코드가 존재하지 않습니다.");
