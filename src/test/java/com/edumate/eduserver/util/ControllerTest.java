50b93d2f88ff8202cc5a928f6fd9b0e1fd2177fb
kgy1008 <gayeon1008@g.skku.edu>
2025-06-12 23:06:55 +0900

    [feat] 테스트 환경에서 인증 없이 접근 가능하도록 설정 추가

--- src/test/java/com/edumate/eduserver/util/ControllerTest.java
+++ src/test/java/com/edumate/eduserver/util/ControllerTest.java
@@ -1,15 +1,18 @@
 package com.edumate.eduserver.util;
 
+import com.edumate.eduserver.config.TestSecurityConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
 @ActiveProfiles("test")
 @AutoConfigureRestDocs
+@Import(TestSecurityConfig.class)
 public class ControllerTest {
 
     @Autowired
