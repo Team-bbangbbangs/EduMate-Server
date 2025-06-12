f961f8d0b4a8c79f52ea6ba09c2a4a0d7351871e
kgy1008 <gayeon1008@g.skku.edu>
2025-06-12 23:55:32 +0900

    [refac] 사용하지 않는 함수 삭제

--- src/main/java/com/edumate/eduserver/studentrecord/repository/StudentRecordDetailRepository.java
+++ src/main/java/com/edumate/eduserver/studentrecord/repository/StudentRecordDetailRepository.java
@@ -3,12 +3,9 @@ package com.edumate.eduserver.studentrecord.repository;
 import com.edumate.eduserver.studentrecord.domain.MemberStudentRecord;
 import com.edumate.eduserver.studentrecord.domain.StudentRecordDetail;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRecordDetailRepository extends JpaRepository<StudentRecordDetail, Long> {
-
-    Optional<StudentRecordDetail> findByIdAndMemberStudentRecord(long recordId, MemberStudentRecord memberStudentRecord);
 
     List<StudentRecordDetail> findAllByMemberStudentRecordOrderByCreatedAtAsc(MemberStudentRecord memberStudentRecord);
 }
