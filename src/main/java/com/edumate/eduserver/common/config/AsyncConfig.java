9b8d78d69baec97beebccad50a8e9fe6ae14dbd8
kgy1008 <gayeon1008@g.skku.edu>
2025-06-17 22:56:52 +0900

    [feat] 비동기 처리를 위한 비동기 설정 추가

---
+++ src/main/java/com/edumate/eduserver/common/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.edumate.eduserver.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+}
