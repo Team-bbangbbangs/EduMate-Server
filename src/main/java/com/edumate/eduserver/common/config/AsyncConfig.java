46780d2a37c994d801627d1c4bced37a94c52c7a
kgy1008 <gayeon1008@g.skku.edu>
2025-06-18 00:47:03 +0900

    [refac] 스레드 폭발 방지를 위한 Executor 빈 정의

--- src/main/java/com/edumate/eduserver/common/config/AsyncConfig.java
+++ src/main/java/com/edumate/eduserver/common/config/AsyncConfig.java
@@ -1,9 +1,23 @@
 package com.edumate.eduserver.common.config;
 
+import java.util.concurrent.Executor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableAsync
 @Configuration
 public class AsyncConfig {
+
+    @Bean(name = "emailTaskExecutor")
+    public Executor emailTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(Runtime.getRuntime().availableProcessors());
+        executor.setMaxPoolSize(Runtime.getRuntime().availableProcessors() * 2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("email-async-");
+        executor.initialize();
+        return executor;
+    }
 }
