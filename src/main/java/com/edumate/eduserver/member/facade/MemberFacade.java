6c7489181c80e889c394626bb5f69c94ef4e8508
TaegeunYou <dbxorms2310@naver.com>
2025-06-17 22:01:36 +0900

    [fix] 마지막 생활기록부 메타 정보를 가져오게끔 수정

--- src/main/java/com/edumate/eduserver/member/facade/MemberFacade.java
+++ src/main/java/com/edumate/eduserver/member/facade/MemberFacade.java
@@ -3,6 +3,8 @@ package com.edumate.eduserver.member.facade;
 import com.edumate.eduserver.member.domain.Member;
 import com.edumate.eduserver.member.facade.response.MemberProfileGetResponse;
 import com.edumate.eduserver.member.service.MemberService;
+import com.edumate.eduserver.studentrecord.domain.MemberStudentRecord;
+import com.edumate.eduserver.studentrecord.service.StudentRecordService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,14 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberFacade {
 
     private final MemberService memberService;
-
-    private static final String DEFAULT_SEMESTER = "2025-1";
+    private final StudentRecordService studentRecordService;
 
     public MemberProfileGetResponse getMemberProfile(final long memberId) {
         Member member = memberService.getMemberById(memberId);
+        MemberStudentRecord memberStudentRecord = studentRecordService.getLatestMemberStudentRecord(memberId);
         return MemberProfileGetResponse.of(
                 member.getEmail(), member.getSubject().getName(), member.isVerifyTeacher(),
-                member.getSchool().getName(), member.getNickname(), DEFAULT_SEMESTER
+                member.getSchool().getName(), member.getNickname(), memberStudentRecord.getSemester()
         );
     }
 }
