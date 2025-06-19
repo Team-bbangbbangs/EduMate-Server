ff74704c3210b9c5f9ffecc434e518e267276cd0
kgy1008 <gayeon1008@g.skku.edu>
2025-06-19 22:11:44 +0900

    [refac] 상수명 변경

--- src/main/java/com/edumate/eduserver/auth/service/AuthService.java
+++ src/main/java/com/edumate/eduserver/auth/service/AuthService.java
@@ -23,7 +23,7 @@ public class AuthService {
     private final MemberRepository memberRepository;
 
     private static final String EMPTY_REFRESH_TOKEN = null;
-    private static final boolean NOT_WITHDRAWN = false;
+    private static final boolean NOT_DELETED = false;
 
     @Transactional
     public void verifyEmailCode(final Member member, final String inputCode) {
@@ -44,7 +44,7 @@ public class AuthService {
     }
 
     public void checkAlreadyRegistered(final String email) {
-        if (memberRepository.existsByEmailAndIsDeleted(email, NOT_WITHDRAWN)) {
+        if (memberRepository.existsByEmailAndIsDeleted(email, NOT_DELETED)) {
             throw new MemberAlreadyRegisteredException(AuthErrorCode.MEMBER_ALREADY_REGISTERED);
         }
     }
