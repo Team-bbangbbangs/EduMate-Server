5f17d1a539de5cd092d05273a8963f5773d86b0e
kgy1008 <gayeon1008@g.skku.edu>
2025-06-24 13:36:47 +0900

    [feat] join 조건 명시

--- src/main/java/com/edumate/eduserver/studentrecord/domain/RecordPrompt.java
+++ src/main/java/com/edumate/eduserver/studentrecord/domain/RecordPrompt.java
@@ -6,6 +6,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -23,6 +24,7 @@ public class RecordPrompt {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_record_detail_id", nullable = false)
     private StudentRecordDetail studentRecordDetail;
 
     @Column(nullable = false)
