d8398e74d970b512d7c7894641529f11198b97a7
kgy1008 <gayeon1008@g.skku.edu>
2025-06-24 21:10:17 +0900

    [refac] ResponseEntityExceptionHandler활용

--- src/main/java/com/edumate/eduserver/common/advice/GlobalExceptionHandler.java
+++ src/main/java/com/edumate/eduserver/common/advice/GlobalExceptionHandler.java
@@ -3,17 +3,23 @@ package com.edumate.eduserver.common.advice;
 import com.edumate.eduserver.common.ApiResponse;
 import com.edumate.eduserver.common.exception.EduMateCustomException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import software.amazon.awssdk.core.exception.SdkException;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EduMateCustomException.class)
     public ApiResponse<Void> handleCustomException(final EduMateCustomException e) {
@@ -31,11 +37,22 @@ public class GlobalExceptionHandler {
         return ApiResponse.fail(HttpStatus.BAD_REQUEST.value(), "EDMT-4001", e.getMessage());
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ApiResponse<Void> handleValidationException(final MethodArgumentNotValidException e) {
-        FieldError fieldError = e.getFieldError();
-        String message = (fieldError != null) ? fieldError.getDefaultMessage() : e.getMessage();
-        return ApiResponse.fail(HttpStatus.BAD_REQUEST.value(), "EDMT-4002", message);
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex,
+                                                                  final HttpHeaders headers,
+                                                                  final HttpStatusCode status,
+                                                                  final WebRequest request) {
+        FieldError fieldError = ex.getFieldError();
+        String message = (fieldError != null) ? fieldError.getDefaultMessage() : "유효하지 않은 요청입니다.";
+        return new ResponseEntity<>(ApiResponse.fail(status.value(), "EDMT-4002", message), headers, status);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(final MissingServletRequestParameterException ex,
+                                                                          final HttpHeaders headers,
+                                                                          final HttpStatusCode status,
+                                                                          final WebRequest request) {
+        return new ResponseEntity<>(ApiResponse.fail(status.value(), "EDMT-4001", ex.getMessage()), headers, status);
     }
 
     @ExceptionHandler(SdkException.class)
