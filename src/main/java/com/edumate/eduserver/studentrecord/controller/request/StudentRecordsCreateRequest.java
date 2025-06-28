bb57693b9f9ee573b291fe6c1333d1b34d025670
kgy1008 <gayeon1008@g.skku.edu>
2025-06-28 21:19:09 +0900

    [refac] 입력 크기 제한

--- src/main/java/com/edumate/eduserver/studentrecord/controller/request/StudentRecordsCreateRequest.java
+++ src/main/java/com/edumate/eduserver/studentrecord/controller/request/StudentRecordsCreateRequest.java
@@ -3,11 +3,13 @@ package com.edumate.eduserver.studentrecord.controller.request;
 import com.edumate.eduserver.studentrecord.controller.request.vo.StudentRecordInfo;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record StudentRecordsCreateRequest(
         @NotNull(message = "학기 정보는 필수입니다.")
         String semester,
+        @Size(min = 1, message = "최소 1명 이상의 학생 정보를 입력해주세요.")
         @NotNull(message = "추가할 생기부 목록 리스트는 필수입니다.")
         List<@Valid StudentRecordInfo> studentRecords
 ) {
