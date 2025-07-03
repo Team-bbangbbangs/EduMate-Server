9064f5f5241633870f41b6e4374a1b432a36f88e
kgy1008 <gayeon1008@g.skku.edu>
2025-07-04 03:50:55 +0900

    [fix] remove /actuator/info from security whitelist

--- src/main/java/com/edumate/eduserver/common/config/SecurityConfig.java
+++ src/main/java/com/edumate/eduserver/common/config/SecurityConfig.java
@@ -50,8 +50,7 @@ public class SecurityConfig {
             "/api/v1/auth/verify-email",
             "/api/v1/auth/find-password",
             "/actuator/health",
-            "/actuator/prometheus",
-            "/actuator/info"
+            "/actuator/prometheus"
     };
     private static final String[] BUSINESS_WHITE_LIST = {
             "/api/v1/notices",
