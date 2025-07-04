bd93ca9d8f08860567afc15f3874a6d909610cce
TaegeunYou <dbxorms2310@naver.com>
2025-07-04 14:00:37 +0900

    [fix] request valid 어노테이션 추가

--- src/main/java/com/edumate/eduserver/studentrecord/controller/request/StudentRecordCreateRequest.java
+++ src/main/java/com/edumate/eduserver/studentrecord/controller/request/StudentRecordCreateRequest.java
@@ -1,12 +1,14 @@
 package com.edumate.eduserver.studentrecord.controller.request;
 
 import com.edumate.eduserver.studentrecord.controller.request.vo.StudentRecordCreateInfo;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 public record StudentRecordCreateRequest(
         @NotNull(message = "학기 정보는 필수입니다.")
         String semester,
         @NotNull(message = "추가할 생기부는 필수입니다.")
+        @Valid
         StudentRecordCreateInfo studentRecord
 ) {
 }
